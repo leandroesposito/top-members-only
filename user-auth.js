644b52795032468fdd974aed6ef3efb0d969c921
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-10-18 18:30:36 -0300

    Create user auth functions

---
+++ user-auth.js
@@ -0,0 +1,38 @@
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const userDB = require("./db/user");
+
+const strategy = new LocalStrategy(async (username, password, done) => {
+  try {
+    const user = await userDB.getUserByUsername(username);
+    if (!user) {
+      return done(null, false, { message: "Incorrect username or password" });
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return done(null, false, { message: "Incorrect username or password" });
+    }
+    return done(null, user);
+  } catch (error) {
+    return done(error);
+  }
+});
+
+const serializeUser = (user, done) => {
+  done(null, user.id);
+};
+
+const deserializeUser = async (id, done) => {
+  try {
+    const user = await userDB.getUserById(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+};
+
+module.exports = {
+  strategy,
+  serializeUser,
+  deserializeUser,
+};
