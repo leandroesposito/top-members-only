bce05607b70d07f42f426990529d5d7c4c90c326
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-10-18 18:31:09 -0300

    Implement authentication and session

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 const path = require("node:path");
 const express = require("express");
 require("dotenv").config();
+const session = require("express-session");
+const passport = require("passport");
+const { strategy, serializeUser, deserializeUser } = require("./user-auth");
+const flash = require("connect-flash");
 
 const app = express();
 const signUpRoute = require("./routes/sign_up");
+const logInRouter = require("./routes/log_in");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 
+passport.use(strategy);
+passport.serializeUser(serializeUser);
+passport.deserializeUser(deserializeUser);
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.use("/sign-up", signUpRoute);
+app.use("/log-in", logInRouter);
+app.use("/", (req, res) => {
+  res.status(200).render("index.ejs");
+});
 
 app.listen(process.env.PORT, (error) => {
   if (error) {
