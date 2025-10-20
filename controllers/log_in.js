939701caee4a2edbd2d8fe8d80a67be41fff84e0
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-10-20 15:16:53 -0300

    Remove flash messages extraction

--- controllers/log_in.js
+++ controllers/log_in.js
@@ -1,17 +1,6 @@
 const passport = require("passport");
 
 function logInGet(req, res) {
-  const flashErrors =
-    req.flash && typeof req.flash === "function" ? req.flash("error") : [];
-  if (flashErrors.length > 0) {
-    if (!res.locals.errors) {
-      res.locals.errors = [];
-    }
-    for (const message of flashErrors) {
-      res.locals.errors.push({ msg: message });
-    }
-  }
-
   res.status(200).render("log_in.ejs", { title: "Log-in" });
 }
 
